7ab35a8fb3d0fe49841383d0479ff9674215e41f
arxers <arxers@gmail.com>
2025-02-18 12:23:19 +0800

    made constructors public

--- inc/Message.hpp
+++ inc/Message.hpp
@@ -6,18 +6,19 @@
 
 class Message {
 private:
-    std::string                 prefix;
-    std::string                 command;
-    std::vector<std::string>    params;
+    std::string                 _prefix;
+    std::string                 _command;
+    std::vector<std::string>    _params;
 
     Message();
-    Message(const Message&);
-    Message& operator=(const Message&);
 public:
     std::string                 getPrefix() const;
     std::string                 getCommand() const;
     std::vector<std::string>    getParams() const;
+    bool                        isValid() const;
 
     Message(const std::string&);
+    Message(const Message&);
+    Message& operator=(const Message&);
     ~Message();
 };
--- src/Message.cpp
+++ src/Message.cpp
@@ -1,11 +1,14 @@
 #include "../inc/Message.hpp"
 
 Message::Message(const std::string& msg) {
+    if (msg.empty())
+        return ;
+
     std::istringstream  iss(msg);
 
     if (msg[0] == ':')
-        iss >> this->prefix;
-    if (!(iss >> this->command))
+        iss >> this->_prefix;
+    if (!(iss >> this->_command))
         return ;
 
     std::string token;
@@ -14,18 +17,31 @@ Message::Message(const std::string& msg) {
             std::string trailing;
             std::getline(iss, trailing);
             token = token.substr(1) + trailing;
-            this->params.push_back(token);
+            this->_params.push_back(token);
             break ;
         }
-        this->params.push_back(token);
+        this->_params.push_back(token);
     }
 }
 
-std::string Message::getPrefix() const { return (this->prefix); }
-std::string Message::getCommand() const { return (this->command); }
-std::vector<std::string> Message::getParams() const { return (this->params); }
+std::string Message::getPrefix() const { return (this->_prefix); }
+std::string Message::getCommand() const { return (this->_command); }
+std::vector<std::string> Message::getParams() const { return (this->_params); }
+bool    Message::isValid() const { return (!this->_command.empty()); }
 
 Message::Message() {}
-Message::Message(const Message&) {}
-Message& Message::operator=(const Message&) { return (*this); }
+
+Message::Message(const Message& rhs) {
+    *this = rhs;
+}
+
+Message& Message::operator=(const Message& rhs) {
+    if (this != &rhs) {
+        this->_prefix = rhs._prefix;
+        this->_command = rhs._command;
+        this->_params = rhs._params;
+    }
+    return (*this);
+}
+
 Message::~Message() {}
